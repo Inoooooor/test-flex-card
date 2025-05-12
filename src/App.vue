accc911fbce623f3c2601563e144635ce029ef3c
Inoooooor <111541493+Inoooooor@users.noreply.github.com>
2025-05-12 17:49:00 +0300

    Компонент Алерт дял сообщений

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,119 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref, computed } from 'vue'
+import IconCancel from './components/icons/IconCancel.vue'
+import BaseAlert from './components/BaseAlert.vue'
+
+type Message = {
+  id: number
+  description: string
+  date: string
+  isCanceled: boolean
+}
+
+const messages = ref<Message[]>([
+  {
+    id: 1,
+    description: 'Сообщение 1',
+    date: '2000-20-02',
+    isCanceled: false,
+  },
+  {
+    id: 2,
+    description: 'Сообщение 2',
+    date: '2000-20-02',
+    isCanceled: false,
+  },
+  {
+    id: 3,
+    description: 'Сообщение 3',
+    date: '2000-20-02',
+    isCanceled: false,
+  },
+])
+
+const currentMessageIndex = ref(0)
+
+const allowedMessages = computed(() => {
+  return messages.value.filter((message) => !message.isCanceled)
+})
+
+const currentMessage = computed(() => {
+  const newCurrentIndex = (currentMessageIndex.value + 1) % messages.value.length
+  return allowedMessages.value[newCurrentIndex] || 'Ничего'
+})
+
+const messageInput = ref<string>('')
+
+const addMessage = () => {
+  if (!messageInput.value) {
+    return
+  }
+
+  messages.value.push({
+    id: Date.now(),
+    description: messageInput.value,
+    date: 'Date.now()',
+    isCanceled: false,
+  })
+
+  messageInput.value = ''
+}
+
+const submit = (event: Event) => {
+  event.preventDefault()
+  addMessage()
+}
+
+const cancelMessage = (id: Message['id']) => {
+  const message = messages.value.find((message) => message.id === id)
+
+  if (!message) {
+    return
+  }
+  message.isCanceled = true
+}
+
+const messageTimer = 2000
+setInterval(() => currentMessageIndex.value++, messageTimer)
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div class="container w-50 mx-auto">
+    <base-alert
+      :message="currentMessage"
+      @cancel-message="cancelMessage(currentMessage.id)"
+    ></base-alert>
+    <!-- <div class="row">
+      <p v-for="message in messages" :key="message.id">
+        {{ message.isCanceled }}. {{ message.description }}
+      </p>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    <div class="row">
+      <form @submit="submit">
+        <div class="row">
+          <div class="col-8">
+            <input
+              v-model="messageInput"
+              type="text"
+              class="form-control"
+              placeholder="Сообщение"
+              aria-label="Сообщение"
+            />
+          </div>
+          <div class="col-4">
+            <button
+              type="submit"
+              class="form-control"
+              placeholder="Last name"
+              aria-label="Last name"
+            >
+              Отправить
+            </button>
+          </div>
+        </div>
+      </form>
+    </div> -->
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style scoped></style>
---
+++ src/components/BaseAlert.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import IconCancel from './icons/IconCancel.vue'
+type Message = {
+  id: number
+  description: string
+  date: string
+  isCanceled: boolean
+}
+
+defineProps<{ message: Message }>()
+
+defineEmits(['cancelMessage'])
+</script>
+<template>
+  <div class="row px-3 py-4 rounded-2 alert-bg">
+    <button
+      @click="$emit('cancelMessage')"
+      class="col-1 p-0 d-flex justify-content-center align-items-center border-0 no-bg"
+    >
+      <IconCancel />
+    </button>
+    <span class="col">{{ message.description }}</span>
+  </div>
+</template>
+<style>
+.alert-bg {
+  background-color: #e4f0ed;
+}
+
+.no-bg {
+  background-color: transparent;
+}
+</style>
